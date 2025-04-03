03b59b574d9918e0b9a20db069ec2245ffaabb3d
almerion <tig2468.a@gmail.com>
2025-04-03 09:19:56 +0200

    added fitness

--- genetic/src/path.c
+++ genetic/src/path.c
@@ -3,7 +3,20 @@
 
 #define LIMIT_TIME 180
 
-double evaluateFitness(Matrix matrix,lst adrs); // Tiago
+double evaluateFitness(Matrix matrix,lst adrs){ // Tiago
+    Distime total = {0, 0};
+    
+    while (adrs->next != NULL){
+        int from = adrs->pharmacyIndex;
+        int to = adrs->next->pharmacyIndex;
+
+        Distime current = matrix.matrix[from][to];
+        total.dist += current.dist;
+        total.time += current.time;
+    }
+
+    return total.dist * (total.time > LIMIT_TIME ? 10 : 1);
+}
 
 Path generatePath(Matrix matrix,lst startAdr){
     
